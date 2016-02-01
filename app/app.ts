af725090267d76c1905fc83881cf9787464b8d2a
Chris Lankford <christopherlankford@gmail.com>
2016-01-31 22:19:07 -0800

    Chat client coming soon

---
+++ app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'app',
+  template: `
+    Chat client coming soon!
+  `
+})
+export class App {
+
+  constructor() {
+  }
+}
