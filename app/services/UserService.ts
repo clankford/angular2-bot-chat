a3a72e99a4ce2388c127d958963d6ae30935d9f0
Chris Lankford <christopherlankford@gmail.com>
2016-02-03 17:50:04 -0800

    Added setter for current user

--- app/services/UserService.ts
+++ app/services/UserService.ts
@@ -1,6 +1,12 @@
 import { Injectable } from 'angular2/core';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { User } from '../models/user';
 
-@Injectable
+@Injectable()
 export class UserService {
+  currentUser: Subject<User> = new BehaviorSubject<User>(null);
 
+  public setCurrentUser(newUser: User): void {
+    this.currentUser.next(newUser);
+  }
 }
