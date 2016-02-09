54be88c12cbf7f04f2dee2f9fa12434084dcdfef
Chris Lankford <christopherlankford@gmail.com>
2016-02-08 19:06:35 -0800

    Added ThreadService test functionality, currently incomplete

---
+++ test/services/ThreadService.spec.ts
@@ -0,0 +1,8 @@
+import { User } from '../../app/models/user';
+import { Thread } from '../../app/models/thread';
+
+let chris: User = new User('Chris Lankford', '');
+let ryan: User = new User('Ryan Smith', '');
+
+let t1: Thread = new Thread('t1', 'Thread 1', '');
+let t2: Thread = new Thread('t2', 'Thread 2', '');
