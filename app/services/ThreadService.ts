f302af9c2d97e68bba10931ffd883e16c3e26f98
Chris Lankford <christopherlankford@gmail.com>
2016-02-10 17:13:12 -0800

    Completed the initial implementation of ThreadService

--- app/services/ThreadService.ts
+++ app/services/ThreadService.ts
@@ -9,6 +9,13 @@ import * as _ from 'underscore';
 export class ThreadService {
   // 'threads' is an observable that contains the most up to date list of threads
   threads: Observable<{ [key: string]: Thread }>;
+  // 'orderedThreads' contains a newest-first chronological list of threads
+  orderedThreads: Observable<Thread[]>;
+  // 'currentThread' contains the currently selected thread
+  currentThread: Subject<Thread> = new BehaviorSubject<Thread>(new Thread());
+  // 'currentThreadMessages' contains the set of messages for the currently
+  // selected thread
+  currentThreadMessages: Observable<Message[]>;
 
   constructor(public messageService: MessageService) {
 
@@ -29,6 +36,34 @@ export class ThreadService {
         });
         return threads;
       });
+
+    this.orderedThreads = this.threads
+      .map((threadGroups: { [key: string]: Thread }) => {
+        let threads: Thread[] = _.values(threadGroups);
+        return _.sortBy(threads, (t: Thread) => t.lastMessage.sentAt).reverse();
+      });
+
+    this.currentThreadMessages = this.currentThread
+      .combineLatest(messageService.messages,
+                    (currentThread: Thread, messages: Message[]) => {
+        if (currentThread && messages.length > 0) {
+          return _.chain(messages)
+            .filter((message: Message) =>
+                    (message.thread.id === currentThread.id))
+            .map((message: Message) => {
+              message.isRead = true;
+              return message; })
+            .value();
+        } else {
+          return [];
+        }
+      });
+
+      this.currentThread.subscribe(this.messageService.markThreadAsRead);
+  }
+
+  setCurrentThread(newThread: Thread): void {
+    this.currentThread.next(newThread);
   }
 }
 
