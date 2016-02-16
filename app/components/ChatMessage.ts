c34413df79522abbcfd5bd635b10dd4ac99d78bb
Chris Lankford <christopherlankford@gmail.com>
2016-02-15 16:47:19 -0800

    Added base functionality for Chat Window

---
+++ app/components/ChatMessage.ts
@@ -0,0 +1,11 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'chat-message',
+  template: `
+    chat-message template
+  `
+})
+export class ChatMessage{
+
+}
--- app/components/ChatWindow.ts
+++ app/components/ChatWindow.ts
@@ -1,11 +1,23 @@
-import { Component } from 'angular2/core';
+import { Component, ChangeDetectionStrategy } from 'angular2/core';
+import { FORM_DIRECTIVES } from 'angular2/common';
+import { Observable } from 'rxjs';
+import { ChatMessage } from './ChatMessage';
+import { Thread } from '../models/thread';
+import { Message } from '../models/message';
+import { User } from '../models/user';
 
 @Component({
   selector: 'chat-window',
+  directives: [ChatMessage,
+               FORM_DIRECTIVES],
+  changeDetection: ChangeDetectionStrategy.OnPushObserve,
   template: `
     chat-window component
   `
 })
 export class ChatWindow {
-
+  messages: Observable<any>;
+  currentThread: Thread;
+  draftMessage: Message;
+  currentUser: User;
 }
