c5a4354d78b5d4a7de785cfe37701a6e5e5118a6
Chris Lankford <christopherlankford@gmail.com>
2016-02-15 17:01:09 -0800

    Added implementation for OnInit to set values when the component is loaded.

--- app/components/ChatWindow.ts
+++ app/components/ChatWindow.ts
@@ -1,10 +1,17 @@
-import { Component, ChangeDetectionStrategy } from 'angular2/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  OnInit,
+  ElementRef } from 'angular2/core';
 import { FORM_DIRECTIVES } from 'angular2/common';
 import { Observable } from 'rxjs';
 import { ChatMessage } from './ChatMessage';
 import { Thread } from '../models/thread';
 import { Message } from '../models/message';
 import { User } from '../models/user';
+import { MessageService } from '../services/MessageService';
+import { ThreadService } from '../services/ThreadService';
+import { UserService } from '../services/UserService';
 
 @Component({
   selector: 'chat-window',
@@ -15,9 +22,31 @@ import { User } from '../models/user';
     chat-window component
   `
 })
-export class ChatWindow {
+export class ChatWindow implements OnInit {
   messages: Observable<any>;
   currentThread: Thread;
   draftMessage: Message;
   currentUser: User;
+
+  constructor(public messageService: MessageService,
+              public threadService: ThreadService,
+              public userService: UserService,
+              public el: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    this.messages = this.threadService.currentThreadMessages;
+
+    this.draftMessage = new Message();
+
+    this.threadService.currentThread.subscribe(
+      (thread: Thread) => {
+        this.currentThread = thread;
+      });
+
+    this.userService.currentUser.subscribe(
+      (user: User) => {
+        this.currentUser = user;
+      });
+  }
 }
