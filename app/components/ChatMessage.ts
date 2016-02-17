314a51744aea8184b8afe9c7daefa6013b7e6ee3
Chris Lankford <christopherlankford@gmail.com>
2016-02-16 19:42:24 -0800

    Added moment references and typings.

--- app/components/ChatMessage.ts
+++ app/components/ChatMessage.ts
@@ -1,7 +1,10 @@
 import { Component } from 'angular2/core';
+import { FromNowPipe } from '../util/FromNowPipe';
 
 @Component({
   selector: 'chat-message',
+  inputs: ['message'],
+  pipes: [FromNowPipe],
   template: `
     chat-message template
   `
