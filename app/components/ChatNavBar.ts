ce0d5314f213137110fa809ba47e24fbefc79755
Chris Lankford <christopherlankford@gmail.com>
2016-02-20 11:11:38 -0800

    Added controllor code for the nav bar and logic to count total unread messages

--- app/components/ChatNavBar.ts
+++ app/components/ChatNavBar.ts
@@ -1,4 +1,9 @@
 import { Component } from 'angular2/core';
+import { MessageService } from '../services/MessageService';
+import { ThreadService } from '../services/ThreadService';
+import { Message } from '../models/message';
+import { Thread } from '../models/thread';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'nav-bar',
@@ -7,5 +12,33 @@ import { Component } from 'angular2/core';
   `
 })
 export class ChatNavBar {
+  unreadMessagesCount: number;
 
+  constructor(public messageService: MessageService,
+              public threadService: ThreadService) {
+  }
+
+  ngOnInit(): void {
+    this.messageService.messages
+      .combineLatest(
+        this.threadService.currentThread,
+        (messages: Message[], currentThread: Thread) =>
+          [currentThread, messages] )
+
+      .subscribe(([currentThread, messages]: [Thread, Message[] ]) => {
+        this.unreadMessagesCount =
+          _.reduce(
+            messages,
+            (sum: number, m: Message) => {
+              let messageIsInCurrentThread: boolean = m.thread &&
+                currentThread &&
+                (currentThread.id === m.thread.id);
+                if (m && !m.isRead && !messageIsInCurrentThread) {
+                  sum = sum + 1;
+                }
+                return sum;
+              },
+              0);
+      });
+  }
 }
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,3 @@
 /// <reference path="underscore/underscore.d.ts" />
+/// <reference path="moment/moment.d.ts" />
+/// <reference path="moment/moment-node.d.ts" />
